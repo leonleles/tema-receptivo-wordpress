36cbbdd5311c834c34195c3b021cf0abaed6e5c6
leonleles <leoleles9159@hotmail.com>
2020-09-28 16:31:02 -0300

    Widgets salvando

--- modules/widgets/banners.php
+++ modules/widgets/banners.php
@@ -22,14 +22,29 @@ class NW_Widget_Banners extends WP_Widget {
                 </ul>
             </div>
         </div>
-    <?php
+        <?php
     }
 
-    function update($new_instance, $old_instance) {
-        return $new_instance;
+    // Widget Backend
+    public function form($instance) {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = __('New title', 'wpb_widget_domain');
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+                   name="<?php echo $this->get_field_name('title'); ?>" type="text"
+                   value="<?php echo esc_attr($title); ?>"/>
+        </p>
+        <?php
     }
 
-    function form($instance) {
-        return '';
+    public function update($new_instance, $old_instance) {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
     }
 }
